a79c5c311e33bb9bce4efb36d32d488af671e21b
JoanixX <alvaradocjosorio@gmail.com>
2025-03-19 18:48:09 -0500

    Agregué la página para iniciar sesión con el formGroup y el mat-label se superpone en el input

--- proyectos/angular-1/src/app/pages/auth/login/login.component.ts
+++ proyectos/angular-1/src/app/pages/auth/login/login.component.ts
@@ -1,20 +1,55 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  email!: string;
-  password!: string;
 
+  loginForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private snackBar = inject(MatSnackBar);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 5000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+  
+  controlHasError (controlName: string, error: string){
+    return this.loginForm.controls[controlName].hasError(error);
+  }
   onSubmit() {
-    if (this.email && this.password){
-      console.log('Email: ', this.email, 'Password', this.password);
+    if (this.loginForm.valid) {
+      console.log('Credenciales: ', this.loginForm.value);
+      this.openSnackBar('Inicio de sesión exitoso');
     }
   }
 }
