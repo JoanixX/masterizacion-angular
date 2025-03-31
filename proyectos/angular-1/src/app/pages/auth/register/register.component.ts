05545fbef7e49d59093ca7c7f89eda0401403dc4
JoanixX <alvaradocjosorio@gmail.com>
2025-03-30 21:08:41 -0500

    Intento de mejora del css (no mejoró)

--- proyectos/angular-1/src/app/pages/auth/register/register.component.ts
+++ proyectos/angular-1/src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    RouterLink,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
 
+  registerForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private snackBar = inject(MatSnackBar);
+
+  constructor() {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    }, { validators: this.passwordsMatchValidator });
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 5000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  controlHasError(controlName: string, error: string) {
+    return this.registerForm.controls[controlName].hasError(error);
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Registro exitoso: ', this.registerForm.value);
+      this.openSnackBar('Registro exitoso');
+    }
+  }
+
+  private passwordsMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { match: true };
+  }
 }
